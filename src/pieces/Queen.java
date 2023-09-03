4a536d78da9d2795856571e22f3c3120226a376d
Sheel Patel <115194165+Kyoko522@users.noreply.github.com>
2023-09-03 08:27:07 -0400

    Queen collision and movement added

--- src/pieces/Queen.java
+++ src/pieces/Queen.java
@@ -17,4 +17,72 @@ public class Queen extends Piece {
 
         this.sprite = sheet.getSubimage(1 * sheetScale, isWhite ? 0 : sheetScale, sheetScale, sheetScale).getScaledInstance(board.tileSize, board.tileSize, BufferedImage.SCALE_SMOOTH);
     }
+
+    public boolean isValidMovement(int col, int row) {
+        return Math.abs(this.col - col) == Math.abs(this.row - row) || this.col == col || this.row == row;
+    }
+
+    public boolean moveCollidesWithPiece(int col, int row) {
+
+        if (this.col > col) { // left
+            for (int c = this.col - 1; c > col; c--)
+                if (board.getPiece(c, this.row) != null)
+                    return true;
+        }
+
+        if (this.col < col) { // right
+            for (int c = this.col + 1; c < col; c++)
+                if (board.getPiece(c, this.row) != null)
+                    return true;
+        }
+
+        if (this.row > row) {  // up
+            for (int r = this.row - 1; r > row; r--) {
+                if (board.getPiece(this.col, r) != null)
+                    return true;
+            }
+        }
+
+        if (this.row < row) { // down
+            for (int r = this.row + 1; r < row; r++) {
+                if (board.getPiece(this.col, r) != null)
+                    return true;
+            }
+        }
+
+        //up left
+        if (this.col > col && this.row > row) {
+            for (int i = 1; i < Math.abs(this.col - col); i++) {
+                if (board.getPiece(this.col - i, this.row - i) != null)
+                    return true;
+            }
+        }
+
+//        up right
+        if (this.col < col && this.row > row) {
+            for (int i = 1; i < Math.abs(this.col - col); i++) {
+                if (board.getPiece(this.col + i, this.row - i) != null)
+                    return true;
+            }
+        }
+
+//            down left
+
+        if (this.col < col && this.row < row) {
+            for (int i = 1; i < Math.abs(this.col - col); i++) {
+                if (board.getPiece(this.col - i, this.row + i) != null)
+                    return true;
+            }
+        }
+
+        //down right
+        if (this.col < col && this.row < row) {
+            for (int i = 1; i < Math.abs(this.col - col); i++) {
+                if (board.getPiece(this.col + i, this.row + i) != null)
+                    return true;
+            }
+        }
+
+        return false;
+    }
 }
